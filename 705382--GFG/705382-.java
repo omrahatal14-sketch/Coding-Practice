891a9bf30f274c963b86c54d4aa8616a5c1572e4
Om Rahatal <omrahatal14@gmail.com>
2026-07-11 21:08:35 +0530

    Added solution - LeetHub

---
+++ 705382--GFG/705382-.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int arraySum(int arr[]) {
+        // code here
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+        }
+        return sum;
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
