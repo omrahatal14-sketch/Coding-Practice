309832cb180e3a1c64c19d1bd7d9d8705589e4a4
Om Rahatal <omrahatal14@gmail.com>
2026-07-11 18:14:37 +0530

    Added solution - LeetHub

---
+++ 704474--GFG/704474-.java
@@ -0,0 +1,12 @@
+class Solution {
+    public static void utility(int x) {
+        for(int a = x ; a>-1; a--){
+           System.out.print(a+" ");
+        }
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
