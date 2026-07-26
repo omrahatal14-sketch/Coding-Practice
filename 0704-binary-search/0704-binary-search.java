e1926206fb09d27bceb0d2541b7b763cd0e89723
Om Rahatal <omrahatal14@gmail.com>
2026-07-26 23:03:13 +0530

    Time: 1 ms (10%), Space: 48.6 MB (14.06%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
