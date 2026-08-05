e4a4a6fe5bf3d88d319868d71737dd49ded75d6b
Om Rahatal <omrahatal14@gmail.com>
2026-08-05 19:30:54 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 701019--GFG/701019-.java
@@ -0,0 +1,10 @@
+class Solution {
+    public void insertAtEnd(ArrayList<Integer> arr, int val) {
+        // code here
+        arr.add(val);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
